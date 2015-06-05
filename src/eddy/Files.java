20273a29bbea69427c4ebc53c43fd16852d9cade
Dmitry Ponyatov <dponyatov@gmail.com>
2015-06-05 18:02:40 +0400

    * Files view
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/eddy/Files.java
+++ src/eddy/Files.java
@@ -3,7 +3,7 @@ package eddy;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
-public class nodes extends ViewPart {
+public class Files extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
